7244f5512eac725638a2e12157649b0eb13def87
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-11-25 19:58:38 +0530

    show 2 cards in mobile

--- src/Component/Home/BestSeller.jsx
+++ src/Component/Home/BestSeller.jsx
@@ -179,7 +179,7 @@ const BestSeller = () => {
           </div>
 
           {/* Products Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-2">
             {visibleProducts.map((product, index) => (
               <div 
                 key={product.id}
