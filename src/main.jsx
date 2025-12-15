d508d1e42ede1830e69ce01145041cb0a76fd85e
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-12-15 19:08:40 +0530

    create wishlist and cart page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { WishlistProvider } from "./Component/Context/WishlistContext.jsx";
+import { CartProvider } from "./Component/Context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <WishlistProvider>
+      <CartProvider>
       <App />
+      </CartProvider>
+    </WishlistProvider>
     </BrowserRouter>
   </StrictMode>
 );
